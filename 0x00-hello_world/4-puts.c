ce1d195aa4c1da2b219e978a59f0082a265fc034
Chidiebee <camuchionu3@gmail.com>
2022-09-18 09:18:51 -0700

    Print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Print a string
+  *
+  * Return: Always return 0 if successful
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
