ba82d963462510b230052b74199c3e9eb1350cad
Chidiebee <camuchionu3@gmail.com>
2022-09-29 00:33:39 -0700

    Set memory values to a constant byte

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char);
+int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
 
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
 
-char *strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
 
