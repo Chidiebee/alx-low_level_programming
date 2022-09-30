59ca0ebb737b2801fd1387eccfa0b7b1091d401b
Chidiebee <camuchionu3@gmail.com>
2022-09-30 02:11:39 -0700

    memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,19 @@
 include "main.h"
 
 /**
-* _memcpy -> Memory copy
-* @dest: the destination memory
-* @src: source memory
-* @n: number of bytes to be copied
-* Return: string copied from source
-*/
+_ * _memcpy -> Memory copy
+_ * @dest: the destination memory
+_ * @src: source memory
+_ * @n: number of bytes to be copied
+_ * Return: string copied from source
+_ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
-		dest[a] = src[a];
+_		dest[a] = src[a];
 
-	return (dest);
+_	return (dest);
 }
 
