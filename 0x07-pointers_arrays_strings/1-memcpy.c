e8948c7961bf82df766607cea96ae211915c7d67
Chidiebee <camuchionu3@gmail.com>
2022-10-01 11:34:07 -0700

    bytes from memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,20 @@
-include "main.h"
-
-/**
-* _memcpy - copies a memory area
-* @dest: memory area to be copied to
-* @src: memory area to be copied from
-* @n: number of bytes to be copied
-* Return: string copied from source
-*/
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i = 0;
-
-	for (; i < n; i++)
-
-		dest[i] = src[i];
-
-	return (dest);
-}
++ include "main.h"
++
++ /**
++ * _memcpy - copies a memory area
++ * @dest: memory area to be copied to
++ * @src: memory area to be copied from
++ * @n: number of bytes to be copied
++ * Return: string copied from source
++ */
++
++ char *_memcpy(char *dest, char *src, unsigned int n)
++ {
++	unsigned int i = 0;
++
++	for (; i < n; i++)
++
++		dest[i] = src[i];
++
++	return (dest);
++ }
